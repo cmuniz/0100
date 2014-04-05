26157128295a45e30e3a8b76371a6e3aa83e712a
cristian <cristian@mail.com>
2014-04-05 10:00:00 +0200

    Creada clase BoxBall

---
+++ BoxBall.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class BoxBall here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class BoxBall
+{
+
+    public BoxBall()
+    {
+
+    }
+
+}
